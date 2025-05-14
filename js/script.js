b0e5d87b4d1e641f9441bbefc65a34274746e633
fe1306 <penedo.fe@gmail.com>
2025-05-13 21:11:46 -0300

    alteração do código

--- js/script.js
+++ js/script.js
@@ -1,25 +1,13 @@
-// isso é um comentario de linha
+function faz_login(event) {
+    event.preventDefault()
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
 
-/*
-isso é um comentario de bloco,
-ou seja,
-para mais de uma linha
-*/
-
-/* Definir o tipo de espaço de memória que eu desejo criar, podedendo ser:
-* "let" para variaveis -> O valor pode ser alterado no decorrer do algoritmo
-
-* "const" para constantes -> Não é possível alterar o valor armazenado neste tipo de dado.
-*/
-
-/*
-Após definir o tipo de dado, precisamos colocar qual será o nome da variável seguindo as regras apresentadas no slide.
-*/
-
-/* 
-Caso seja um dado do tipo let, poderá ser vazio.
-Caso seja um dado do tipo const, precisará ser informado o valor a ser armazenado na constante.
-*/
-
-
-
+    if (email == "tinhos@gmail.com" && senha == "tinho") {
+        alert ("Login realizado com sucesso");
+        window.location.href = "index2.html"
+    }
+    else {
+      alert ("Usuário não cadastrado, reveja seus dados")
+    }
+}
--- script2.js
+++
@@ -1,8 +0,0 @@
-function douglas() {
-    const nome = document.getElementById('marcos').value;
-    const idade = document.getElementById('gabriel').value;
-    alert(nome + ' possui ' + idade + ' anos ');
-    
-}
-
-
